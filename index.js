6cb720a0db2e9c5276eb6d0d42336e9c3be3b565
jacobrieber <jacob@Jacobs-MacBook-Air-2.local>
2022-03-31 17:49:23 -0400

    Build out raw webpages & consolidate JS

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-function onHover() {
-  $("#logo").attr("src", "Photos/Suprised_Logo.svg");
-}
+// Animate logo
+$(function() {
+  $(".animate").on({
 
-function offHover() {
-  $("#logo").attr("src", "Photos/Closed_Logo.svg");
-}
+      mouseenter: function() {
 
+        var loc = window.location.pathname;
+        if (loc.includes("/overviews")) {
+          $(".logo").attr("src", "../../Photos/Suprised_Logo.svg");
+        } else if (loc.includes("/projects")) {
+          $(".logo").attr("src", "../Photos/Suprised_Logo.svg");
+        } else {
+          $(".logo").attr("src", "Photos/Suprised_Logo.svg");
+        }
+      },
 
-$(function(){
-  $(".btn").on({
-   mouseenter: function(){
-    $("#logo").attr("src", "Photos/Suprised_Logo.svg");
-  },
-  mouseleave: function(){
-    $("#logo").attr("src", "Photos/Closed_Logo.svg");
-  }
-  });
-});
-
-$(function(){
-  $(".nav-item").on({
-   mouseenter: function(){
-    $("#logo").attr("src", "Photos/Suprised_Logo.svg");
-  },
-  mouseleave: function(){
-    $("#logo").attr("src", "Photos/Closed_Logo.svg");
-  }
+      mouseleave: function() {
+        var loc = window.location.pathname;
+        if (loc.includes("/overviews")) {
+          $(".logo").attr("src", "../../Photos/Closed_Logo.svg");
+        } else if (loc.includes("/Projects")) {
+          $(".logo").attr("src", "../Photos/Closed_Logo.svg");
+        } else {
+          $(".logo").attr("src", "Photos/Closed_Logo.svg");
+        }
+      },
   });
 });
